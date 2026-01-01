66a3807efce8fcd5d9cc08379e0caf79f4f8d833
UT <marwantl@icloud.com>
2026-01-01 20:32:33 +0100

    Add responsive brand logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ThemeToggle from './ThemeToggle';
 import './Header.css';
+import logoWhite from '../assets/logo-white.png';
+import logoBlack from '../assets/logo-black.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container header-container">
-        <div className="logo">fibre optique <span className="text-accent">france</span></div>
+        <div className="logo-wrapper">
+          <img src={logoWhite} alt="Logo" className="logo-img logo-dark" />
+          <img src={logoBlack} alt="Logo" className="logo-img logo-light" />
+          <div className="logo">fibre optique <span className="text-accent">france</span></div>
+        </div>
         <div className="header-actions">
           <ThemeToggle />
           <a href="tel:+33659896116" className="btn btn-primary btn-sm">
